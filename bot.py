e2d5a199eff5351ff4d12bb8587f1bb517bf0a66
P01nt-Less <mindscriptmc@gmail.com>
2018-08-28 21:19:35 +0100

    yes

--- bot.py
+++ bot.py
@@ -1,23 +1,8 @@
-import discord
 from discord.ext import commands
 import asyncio
 import inspect
 import sys
-import urllib.request
-import zipfile
 import os
-
-REPOSITORY_ZIP_URL = 'https://github.com/Rapptz/discord.py/archive/rewrite.zip#egg=discord.py[voice]'
-filename, headers = urllib.request.urlretrieve(REPOSITORY_ZIP_URL)
-zip = zipfile.ZipFile(filename)
-directory = filename + '_dir'
-zip.extractall(directory)
-module_directory_from_zip = os.listdir(directory)[0]
-module_directory = 'github-discord-rewrite'
-os.rename(os.path.join(directory, module_directory_from_zip),
-          os.path.join(directory, module_directory))
-sys.path.append(directory)
-import github-discord-rewrite
 
 bot = commands.Bot(command_prefix='p.',case_insensitive=True,description='A discord bot.',self_bot=False,owner_id=276043503514025984)
 bot.remove_command('help')
@@ -41,9 +26,9 @@ async def eval(ctx, *, code):
         if inspect.isawaitable(result):
             result = await result
         else:
-            await ctx.send('```py\nInput: {}\nOutput: {}\n```'.format(code, result))
+            await bot.say('```py\nInput: {}\nOutput: {}\n```'.format(code, result))
     except Exception as e:
-            await ctx.send('```py\nInput: {}\n{}: {}```'.format(code, type(e).__name__, e))
+            await bot.say('```py\nInput: {}\n{}: {}```'.format(code, type(e).__name__, e))
 
 @bot.command(pass_context=True, aliases=['commands', 'cmds','h'])
 async def help(ctx,cmd: str=None):
@@ -58,12 +43,12 @@ async def help(ctx,cmd: str=None):
         i.add_field(name='Moderation', value='Nothing')
         i.add_field(name='Owner', value='Nothing')
         i.set_footer(text='Do !help <command> to find out what it does.')
-        await ctx.send(embed=cmd)
+        await bot.say(embed=cmd)
     if cmd:
         get = bot.get_command(cmd)
         s = discord.Embed(title='Help', color=0xFFFF00)
         s.add_field(name=f'Command: ,{cmd}', value=f'Help: {get.help}')
-        return await ctx.send(embed=s)
+        return await bot.say(embed=s)
     else:
         return
 
