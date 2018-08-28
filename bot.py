4b90f1ecb9ffb37ae41609c82f0411771c25b635
P01nt-Less <mindscriptmc@gmail.com>
2018-08-28 21:42:57 +0100

    a

--- bot.py
+++ bot.py
@@ -4,6 +4,10 @@ import asyncio
 import inspect
 import sys
 import os
+import io
+import textwrap
+import aiohttp
+from contextlib import redirect_stdout
 
 bot = commands.Bot(command_prefix='p.',case_insensitive=True,description='A discord bot.',self_bot=False,owner_id=276043503514025984)
 bot.remove_command('help')
@@ -16,19 +20,88 @@ async def on_ready():
     print('Success!')
     await bot.change_presence(game=discord.Game(name=f'over {len(bot.servers)} servers | ,help',type=3))
 
-@commands.check(owner)
-@bot.command(pass_context=True,aliases=['evaluate'])
-async def eval(ctx, *, code):
-    '''Evaluate some code.\n`evaluate`'''
-    code = code.strip('`')
+
+@bot.command(name='eval')
+async def _eval(ctx, *, body):
+    """Evaluates python code"""
+    env = {
+        'ctx': ctx,
+        'bot': bot,
+        'channel': ctx.channel,
+        'author': ctx.author,
+        'guild': ctx.guild,
+        'message': ctx.message,
+        'source': inspect.getsource
+    }
+    def cleanup_code(content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+        # remove `foo`
+        return content.strip('` \n')
+    def get_syntax_error(e):
+        if e.text is None:
+            return f'```py\n{e.__class__.__name__}: {e}\n```'
+        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+    env.update(globals())
+    body = cleanup_code(body)
+    stdout = io.StringIO()
+    err = out = None
+    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+    def paginate(text: str):
+        '''Simple generator that paginates text.'''
+        last = 0
+        pages = []
+        for curr in range(0, len(text)):
+            if curr % 1980 == 0:
+                pages.append(text[last:curr])
+                last = curr
+                appd_index = curr
+        if appd_index != len(text)-1:
+            pages.append(text[last:curr])
+        return list(filter(lambda a: a != '', pages))
     try:
-        result = eval(code)
-        if inspect.isawaitable(result):
-            result = await result
-        else:
-            await bot.say('```py\nInput: {}\nOutput: {}\n```'.format(code, result))
+        exec(to_compile, env)
     except Exception as e:
-            await bot.say('```py\nInput: {}\n{}: {}```'.format(code, type(e).__name__, e))
+        err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+        return await ctx.message.add_reaction('\u2049')
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+    else:
+        value = stdout.getvalue()
+        if ret is None:
+            if value:
+                try:
+                    out = await ctx.send(f'```py\n{value}\n```')
+                except:
+                    paginated_text = paginate(value)
+                    for page in paginated_text:
+                        if page == paginated_text[-1]:
+                            out = await ctx.send(f'```py\n{page}\n```')
+                            break
+                        await ctx.send(f'```py\n{page}\n```')
+        else:
+            try:
+                out = await ctx.send(f'```py\n{value}{ret}\n```')
+            except:
+                paginated_text = paginate(f"{value}{ret}")
+                for page in paginated_text:
+                    if page == paginated_text[-1]:
+                        out = await ctx.send(f'```py\n{page}\n```')
+                        break
+                    await ctx.send(f'```py\n{page}\n```')
+    if out:
+        await ctx.message.add_reaction('\u2705')  # tick
+    elif err:
+        await ctx.message.add_reaction('\u2049')  # x
+    else:
+        await ctx.message.add_reaction('\u2705')
 
 @bot.command(pass_context=True, aliases=['commands', 'cmds','h'])
 async def help(ctx,cmd: str=None):
