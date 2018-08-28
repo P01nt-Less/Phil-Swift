4f7e858d1206f0f3355ae14298382033b025e84e
P01nt-Less <mindscriptmc@gmail.com>
2018-08-28 22:23:50 +0100

    lol

--- bot.py
+++ bot.py
@@ -20,80 +20,6 @@ async def on_ready():
     print('Success!')
     await bot.change_presence(game=discord.Game(name=f'over {len(bot.servers)} servers | ,help',type=3))
 
-
-@bot.command(name='eval')
-async def _eval(ctx, *, body):
-    """Evaluates python code"""
-    def cleanup_code(content):
-        """Automatically removes code blocks from the code."""
-        # remove ```py\n```
-        if content.startswith('```') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
-        # remove `foo`
-        return content.strip('` \n')
-    def get_syntax_error(e):
-        if e.text is None:
-            return f'```py\n{e.__class__.__name__}: {e}\n```'
-        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
-    env.update(globals())
-    body = cleanup_code(body)
-    stdout = io.StringIO()
-    err = out = None
-    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-    def paginate(text: str):
-        '''Simple generator that paginates text.'''
-        last = 0
-        pages = []
-        for curr in range(0, len(text)):
-            if curr % 1980 == 0:
-                pages.append(text[last:curr])
-                last = curr
-                appd_index = curr
-        if appd_index != len(text)-1:
-            pages.append(text[last:curr])
-        return list(filter(lambda a: a != '', pages))
-    try:
-        exec(to_compile, env)
-    except Exception as e:
-        err = await bot.say(f'```py\n{e.__class__.__name__}: {e}\n```')
-        return await ctx.message.add_reaction('\u2049')
-    func = env['func']
-    try:
-        with redirect_stdout(stdout):
-            ret = await func()
-    except Exception as e:
-        value = stdout.getvalue()
-        err = await bot.say(f'```py\n{value}{traceback.format_exc()}\n```')
-    else:
-        value = stdout.getvalue()
-        if ret is None:
-            if value:
-                try:
-                    out = await bot.say(f'```py\n{value}\n```')
-                except:
-                    paginated_text = paginate(value)
-                    for page in paginated_text:
-                        if page == paginated_text[-1]:
-                            out = await bot.say(f'```py\n{page}\n```')
-                            break
-                        await bot.say(f'```py\n{page}\n```')
-        else:
-            try:
-                out = await bot.say(f'```py\n{value}{ret}\n```')
-            except:
-                paginated_text = paginate(f"{value}{ret}")
-                for page in paginated_text:
-                    if page == paginated_text[-1]:
-                        out = await bot.say(f'```py\n{page}\n```')
-                        break
-                    await bot.say(f'```py\n{page}\n```')
-    if out:
-        await ctx.message.add_reaction('\u2705')  # tick
-    elif err:
-        await ctx.message.add_reaction('\u2049')  # x
-    else:
-        await ctx.message.add_reaction('\u2705')
-
 @bot.command(pass_context=True, aliases=['commands', 'cmds','h'])
 async def help(ctx,cmd: str=None):
     '''Get a list of commands.\n`commands` `cmds` `h`'''
@@ -115,6 +41,5 @@ async def help(ctx,cmd: str=None):
         return await bot.say(embed=s)
     else:
         return
-
 
 bot.run(os.environ.get('TOKEN'))
