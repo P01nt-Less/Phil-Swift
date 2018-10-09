ab735f6c07f7172f00b6c14819c820f42adca69e
P01nt-Less <mindscriptmc@gmail.com>
2018-10-09 21:40:10 +0100

    a

--- bot.py
+++ bot.py
@@ -114,8 +114,8 @@ async def suggest(ctx, *, idea):
         osuggest.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
         ssuggest = discord.Embed(title='Suggest',description='Sent that suggestion over! Thank you!',color=0x00FF00)
         ssuggest.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
-        channel = bot.get_channel('431958618791739392')
-        x = await channel.send(embed=osuggest)
+        channell = bot.get_channel('431958618791739392')
+        x = await channell.send(embed=osuggest)
         await x.add_reaction('✅')
         await x.add_reaction('❌')
     else:
