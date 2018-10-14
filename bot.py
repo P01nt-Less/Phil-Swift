fc50459aa75617ee7c4e62fd06e00ecf353b97d5
P01nt-Less <mindscriptmc@gmail.com>
2018-10-14 20:47:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -529,7 +529,7 @@ class BannedMember(commands.Converter):
         return user
 
 @bot.command(pass_context=True, aliases=['ub', 'uban'])
-async def unban(ctx, member: BannedMember, *, reason='The unban hammer has spoken!'):
+async def unban(ctx, member: discord.Member.id, *, reason='The unban hammer has spoken!'):
     '''Unban someone\nUsage: !unban <member> [reason]\nAliases: !ub, !uban\nPermission: Ban Members'''
     if not reason:
         runban = discord.Embed(title='Error', description='You must specify a reason!', color=0xFF0000)
