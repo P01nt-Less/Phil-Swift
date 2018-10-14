c4bdbfe0695556de698590be82a331fe0c0213af
P01nt-Less <mindscriptmc@gmail.com>
2018-10-14 21:19:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -528,6 +528,55 @@ class BannedMember(commands.Converter):
 
         return user
 
+class MemberID(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            m = await commands.MemberConverter().convert(ctx, argument)
+        except commands.BadArgument:
+            try:
+                return int(argument, base=10)
+            except ValueError:
+                raise commands.BadArgument(f"{argument} is not a valid member or member ID.") from None
+        else:
+            can_execute = ctx.author.id == ctx.bot.owner_id or \
+                          ctx.author == ctx.guild.owner or \
+                          ctx.author.top_role > m.top_role
+
+            if not can_execute:
+                raise commands.BadArgument('You cannot do this action on this user due to role hierarchy.')
+            return m.id
+
+class BannedMember(commands.Converter):
+    async def convert(self, ctx, argument):
+        ban_list = await ctx.guild.bans()
+        try:
+            member_id = int(argument, base=10)
+            entity = discord.utils.find(lambda u: u.user.id == member_id, ban_list)
+        except ValueError:
+            entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
+
+        if entity is None:
+            raise commands.BadArgument("Not a valid previously-banned member.")
+        return entity
+
+class ActionReason(commands.Converter):
+    async def convert(self, ctx, argument):
+        ret = f'{ctx.author} (ID: {ctx.author.id}): {argument}'
+
+        if len(ret) > 512:
+            reason_max = 512 - len(ret) - len(argument)
+            raise commands.BadArgument(f'reason is too long ({len(argument)}/{reason_max})')
+        return ret
+@bot.command(pass_context=True)
+@commands.has_permissions(ban_members=True)
+async def unban(self, ctx, member: BannedMember, *, reason: ActionReason = None):
+    if reason is None:
+        reason = f'Action done by {ctx.author} (ID: {ctx.author.id})'
+    await ctx.guild.unban(member.user, reason=reason)
+    if member.reason:
+        await ctx.send(f'Unbanned {member.user} (ID: {member.user.id}), previously banned for {member.reason}.')
+    else:
+        await ctx.send(f'Unbanned {member.user} (ID: {member.user.id}).')
 @bot.command(pass_context=True, aliases=['ub', 'uban'])
 async def unban(ctx):
     await ctx.send('hi there i dont know how i make one. I\'ve taken hours just to find how, and I can\'t, soo uh dm Pointless#1278 cause I need help lul. You\'ll be credited after this.'')
